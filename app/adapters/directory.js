058df3542522c07cbc113683e866a77993cf3bb2
skoslitz <s_koslitz@ifl-leipzig.de>
2016-05-19 15:24:04 +0200

    hack rest adapter: path name not like model name

--- app/adapters/directory.js
+++ app/adapters/directory.js
@@ -2,5 +2,8 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
 	host: 'http://localhost:1313',
-	namespace: 'api/dir'
+	namespace: 'api/dir',
+	pathForType: function(modelName) {
+    	return "reihe";
+  	}
 });
