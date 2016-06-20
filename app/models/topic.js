e10720107e4e28a07edf2919d50b6f037cdf9e8d
skoslitz <sebastian.koslitz@gmail.com>
2016-06-20 13:30:50 +0200

    extent topic model

--- app/models/topic.js
+++ app/models/topic.js
@@ -3,8 +3,14 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	path: DS.attr("string"),
 	title: DS.attr("string"),
+	subtitle: DS.attr("string"),
 	date: DS.attr("string"),
+	author: DS.attr("string"),
 	description: DS.attr("string"),
 	vgWort: DS.attr("string"),
-	content: DS.attr("string")
+	content: DS.attr("string"),
+	cover: DS.attr("string"),
+	coverSrc: DS.attr("string"),
+	coverName: DS.attr("string")
+	//categories: DS.hasMany('category', {async: true }),
 });
--- app/serializers/topic.js
+++ app/serializers/topic.js
@@ -9,10 +9,17 @@ export default DS.JSONSerializer.extend({
 	        attributes: {
 	          path: payload.page.path,
 	          title: payload.page.metadata.title,
+	          subtitle: payload.page.metadata.subtitle,
 	          date: payload.page.metadata.date,
+	          author: payload.page.metadata.autor,
+	          categories: payload.page.metadata.rubriken,
 	          description: payload.page.metadata.description,
 	          vgWort: payload.page.metadata.vg_wort_code,
-	          content: payload.page.content
+	          content: payload.page.content,
+	          cover: payload.page.metadata.titelbild,
+	          coverSrc: payload.page.metadata.titelbild_quelle,
+	          coverName: payload.page.metadata.titelbild_titel,
+
 	        }
 	      }
 	    };
