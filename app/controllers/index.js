bb4596e69913c46eb101d2f51badbf7e3301c744
skoslitz <sebastian.koslitz@gmail.com>
2016-06-28 13:58:51 +0200

    add properties to region list controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,13 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	showPromptDialog : false,
-	newModel: {},
+	id: "",
+	title: "",
+	bandNumberChanged: Ember.observer('id', function() { 
+    	console.log('observer is called', this.get('id')); 
+  	}),	
 	actions: {
 	    openPromptDialog(){
 	    	this.set('showPromptDialog', true) 
+	    	this.set('id', 79) 
 	    },
 	    closePromptDialog(){
-	      this.set('showPromptDialog', false)	      
+	      this.set('showPromptDialog', false)
+	      console.log(this.get('id'))
 	    },
 	    save(){
 	      let region = this.store.createRecord('region', this.get("newModel"))
