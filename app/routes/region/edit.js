8e751b1c0ce9ffbdcb9ac34b57a74a96beba6cd5
Sebastian Koslitz <sebastian.koslitz@gmail.com>
2016-06-29 22:06:55 +0200

    red app bar, grey background

--- app/routes/region/edit.js
+++ app/routes/region/edit.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function(params) {
-    	return this.store.findRecord('region', params.fileName); 
-  	}
+      let model = {}
+      model.region = this.store.findRecord('region', params.fileName);
+      model.excursion = this.store.findAll('excursion-list');
+    	model.topic = this.store.findAll('topic-list');
+      return model
+  }
+
 });
+
