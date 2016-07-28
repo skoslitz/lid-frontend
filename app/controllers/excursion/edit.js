17e2a63ca1a34127e0018070236482163c03404a
skoslitz <sebastian.koslitz@gmail.com>
2016-07-28 14:31:31 +0200

    stick trix editor toolbar to top if window scroll (wip)

--- app/controllers/excursion/edit.js
+++ app/controllers/excursion/edit.js
@@ -1,15 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	// TODO: make this work
+	stickTrixToolbar: Ember.observer('distanceFromTop', function() {
+		console.log('observer is called', this.get('distanceFromTop')); 
+		// stick trix editor toolbar to top after scrolling
+		/*
+			$(window).scroll(function() {
+	        var distanceFromTop = $(document).scrollTop();
+	        if (distanceFromTop >= 740) {
+	            $('#trix-toolbar-1').addClass('sticky-trix-toolbar');
+	        } else {
+	            $('#trix-toolbar-1').removeClass('sticky-trix-toolbar');
+	        }
+    	});*/
+	}),		
 	actions: {
-		handleTrixAction: function(jqEvent) {
+		handleTrixAction: function(jqEvent) {			
 			console.log(jqEvent)
 		},
 		updateExcursion: function() {
 			
 		},
 		showRelatedRegion: function() {
-			this.transitionToRoute('region.edit', `${regionId}-${regionName}.md`);
+			this.transitionToRoute('region.edit', `${regionName}.md`);
 		}
 	}
+	
 });
