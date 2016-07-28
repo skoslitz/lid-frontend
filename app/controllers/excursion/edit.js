591ad6b4c3921ea1d68219c7dc658c561d2fed75
skoslitz <sebastian.koslitz@gmail.com>
2016-07-28 14:43:21 +0200

    stick trix editor toolbar in excursion edit view

--- app/controllers/excursion/edit.js
+++ app/controllers/excursion/edit.js
@@ -1,20 +1,17 @@
 import Ember from 'ember';
 
+	// stick trix editor toolbar to top after scrolling
+	$(window).scroll(function() {
+	    var distanceFromTop = $(document).scrollTop();
+	    if (distanceFromTop >= 689) {
+	        $('#trix-toolbar-1').addClass('sticky-trix-toolbar');
+	    } else {
+	        $('#trix-toolbar-1').removeClass('sticky-trix-toolbar');
+	    }
+	});
+
 export default Ember.Controller.extend({
-	// TODO: make this work
-	stickTrixToolbar: Ember.observer('distanceFromTop', function() {
-		console.log('observer is called', this.get('distanceFromTop')); 
-		// stick trix editor toolbar to top after scrolling
-		/*
-			$(window).scroll(function() {
-	        var distanceFromTop = $(document).scrollTop();
-	        if (distanceFromTop >= 740) {
-	            $('#trix-toolbar-1').addClass('sticky-trix-toolbar');
-	        } else {
-	            $('#trix-toolbar-1').removeClass('sticky-trix-toolbar');
-	        }
-    	});*/
-	}),		
+		
 	actions: {
 		handleTrixAction: function(jqEvent) {			
 			console.log(jqEvent)
