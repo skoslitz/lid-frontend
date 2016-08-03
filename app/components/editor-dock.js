31b384f9539873c0fdd06444e91510b78a0ae92e
skoslitz <sebastian.koslitz@gmail.com>
2016-08-03 11:28:10 +0200

    editor dock as component in every view   - layout adaption for all views

---
+++ app/components/editor-dock.js
@@ -0,0 +1,48 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	createRegion: false,
+	createTopic: false,
+	createExcursion: false,
+	showPromptDialog : false,
+	actions: {
+		openPromptDialog(){
+	    	this.set('showPromptDialog', true) 
+	    },
+	    closePromptDialog(){
+	      	this.set('showPromptDialog', false)
+	    },
+	    saveRegion() {
+			const regionId = this.get('region.id');
+			const regionName = this.get('region.title');
+
+			const newRegion = this.store.createRecord('region', {
+				bandnummer: regionId,
+				title: regionName,
+				path: `regionen/${regionId}-${regionName}.md`,
+				subtitle: "",
+				description: "",
+				date: "",
+				dateOfPublication: "",
+				editor: "",
+				editorInstitut: "",
+				isbn: "",
+				location: "",	
+				editionTitle: "",
+				pagination: 100,
+				extent: [],
+				publisher: "",
+				bundesland: ["Sachsen", "Thüringen"],
+				content: "some content text"
+			});
+
+			newRegion.save().then((response) => {
+				this.set('region.id', '');
+				this.set('region.title', '');
+				this.set('showPromptDialog', false);
+				this.transitionToRoute('region.edit', `${regionId}-${regionName}.md`);				
+			});
+
+	    },				
+	}
+});
