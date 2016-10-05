a63948203b98a75374f9200ba39a7d2d77b30615
Sebastian Koslitz <sebastian.koslitz@gmail.com>
2016-10-05 19:33:11 +0200

    add leaflet map to edit excursion stations

--- app/controllers/excursion/edit.js
+++ app/controllers/excursion/edit.js
@@ -1,17 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-		
 	actions: {
 		getExcursion() {
             console.log(this.get('model.exkursion'));
-        },
+    },
+    updateLocation(excursion, e) {
+      let location = e.target.getLatLng();
+      Ember.setProperties(excursion, {
+        lat: location.lat.toFixed(7),
+        lon: location.lng.toFixed(7)
+      });
+    },
 		updateExcursion: function() {
-			
+
 		},
 		showRelatedRegion: function() {
 			this.transitionToRoute('region.edit', `${regionName}.md`);
 		}
 	}
-	
+
 });
