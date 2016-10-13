8df9915a1f622690e6f39f122211d3d837ca51c0
skoslitz <sebastian.koslitz@gmail.com>
2016-10-13 13:58:46 +0200

    add normalizeQueryResponse for excursion and topic-list

--- app/serializers/excursion-list.js
+++ app/serializers/excursion-list.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
-  
+	normalizeQueryResponse(store, primaryModelClass, payload, id, requestType) {
+	    if (payload.data == null) {
+	    	return {data: []};
+	    } else {
+	        return this._super(...arguments);
+	    }
+  	}
 });
