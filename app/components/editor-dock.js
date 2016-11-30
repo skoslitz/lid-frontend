e2945959caed928b1a36f5dd5edbf79880102637
skoslitz <sebastian.koslitz@gmail.com>
2016-11-30 12:33:13 +0100

    Hugo compile commands show log ouput as string

--- app/components/editor-dock.js
+++ app/components/editor-dock.js
@@ -2,24 +2,50 @@ import Ember from 'ember';
 let { $ } = Ember;
 
 export default Ember.Component.extend({
+	showDialog: false,
+	statusMessage: "",
+	statusPending: true,
 	deletePage: false,
 	actions: {
 		publishSite() {
-			// TODO: check model.hasDirtyAttributes before upload
-			console.log("Send cmd hugo with publish params");
+			// TODO: check model.hasDirtyAttributes before upload			      	
+			let self = this;
+	      	window.scrollTo(0, 0);
+	      	self.set('statusPending', true);
+	      	this.set('showDialog', true);
+	      	this.set('statusMessage', "Seiten werden publiziert.");
 	      	let url = "http://localhost:1313/api/site/publish";
 			return $.ajax({
 				type: 'POST',
 				url: url
+			}).done(function(data, textStatus, jqXHR) {
+				self.set('statusMessage', jqXHR.responseJSON.output);
+				self.set('statusPending', false);				    
+			}).fail(function(jqXHR) {											
+				self.set('statusMessage', jqXHR.responseText);			    
+				self.set('statusPending', false);
 			});
 	    },
 	    previewSite() {
-	      	console.log("Send cmd hugo with preview params");
+	      	let self = this;
+	      	window.scrollTo(0, 0);
+	      	self.set('statusPending', true);
+	      	this.set('showDialog', true);
+	      	this.set('statusMessage', "Vorschau wird erstellt.");
 	      	let url = "http://localhost:1313/api/site/preview";
 			return $.ajax({
 				type: 'POST',
 				url: url
+			}).done(function(data, textStatus, jqXHR) {
+				self.set('statusMessage', jqXHR.responseJSON.output);
+				self.set('statusPending', false);				    
+			}).fail(function(jqXHR) {											
+				self.set('statusMessage', jqXHR.responseText);			    
+				self.set('statusPending', false);
 			});
+	    },
+	    closeDialog() {
+	    	this.set('showDialog', false);
 	    },
 	    deleteRegion(model) {
 	    	this.sendAction('deleteRegionAction', model);
