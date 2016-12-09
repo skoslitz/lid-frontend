124b74ec06530c6efa576a93f04b59e85ccccc49
Sebastian Koslitz <sebastian.koslitz@gmail.com>
2016-12-09 20:29:55 +0100

    page asset menu improvements

--- app/models/region.js
+++ app/models/region.js
@@ -26,7 +26,7 @@ export default DS.Model.extend({
 	assetUrl: Ember.computed('bandnummer', function() {
 		let bandnummer = this.get('bandnummer');
 		return `${'api/asset/img/'+ bandnummer}`
-	})	
+	}),
 
 	// Associations
 	excursions: DS.hasMany('excursion'),
