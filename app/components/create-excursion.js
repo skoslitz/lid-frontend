4afe508cd25d5bc7debf9d567f34d0f8e9363a23
Sebastian Koslitz <sebastian.koslitz@gmail.com>
2016-12-20 11:52:39 +0100

    german umlaut change in create page file name

--- app/components/create-excursion.js
+++ app/components/create-excursion.js
@@ -6,7 +6,30 @@ export default Ember.Component.extend({
 	externalExcursion: false,
 	actionBound: false,
 	actions: {
-		closeCreateExcursionDialog() {
+			umlautCheck() {
+				let value = this.get('articleName') || this.set('articleName', "")
+				console.log(value)
+				switch (event.keyCode) {
+					// ä Umlaut to ae
+					case 222:
+						this.set('articleName', `${value}ae`);
+						event.returnValue = false;
+						break;
+					// ö Umlaut to oe
+					case 186:
+						this.set('articleName', `${value}oe`);
+						event.returnValue = false;
+						break;
+					// ü Umlaut to ue
+					case 219:
+						this.set('articleName', `${value}ue`);
+						event.returnValue = false;
+						break;
+					default:
+						console.log('ok--');
+				}
+			},
+			closeCreateExcursionDialog() {
     		this.sendAction('closeCreateExcursionDialogAction');
 	    },
 	    createExcursionAction() {
--- app/components/create-region.js
+++ app/components/create-region.js
@@ -3,8 +3,32 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	showPromptDialog: false,
 	actions: {
-		openPromptDialog(){
-	    	this.set('showPromptDialog', true) 
+			umlautCheck() {
+				console.log(event);
+				let value = this.get('model.title') || ""
+
+				switch (event.keyCode) {
+					// ä Umlaut to ae
+					case 222:
+						this.set('model.title', `${value}ae`);
+						event.returnValue = false;
+						break;
+					// ö Umlaut to oe
+					case 186:
+						this.set('model.title', `${value}oe`);
+						event.returnValue = false;
+						break;
+					// ü Umlaut to ue
+					case 219:
+						this.set('model.title', `${value}ue`);
+						event.returnValue = false;
+						break;
+					default:
+						console.log('ok--');
+				}
+			},
+			openPromptDialog(){
+	    	this.set('showPromptDialog', true)
 	    },
 	    closePromptDialog() {
 	    	this.set('showPromptDialog', false)
